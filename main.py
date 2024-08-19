60bac00f1cfce5c7058ff62cf677d5d1040d4008
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-19 13:34:27 +0200

    Refactor main.py to improve readability and performance

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-    print(count_words(text))
-    print(count_chars(text))
+    dict_of_chars = count_chars(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words(text)} words in the document")
+    print(convert_dict_to_list_of_dicts(dict_of_chars))
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
 def count_words(text):
     return len(text.split())
+
 
 def count_chars(text):
     text = text.lower()
     dict_of_chars = {}
     for char in text:
-        if char in dict_of_chars:
+        if char in dict_of_chars and char.isalpha():
             dict_of_chars[char] += 1
         else:
-            dict_of_chars[char] = 1
+            if char.isalpha():
+                dict_of_chars[char] = 1
     return dict_of_chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def convert_dict_to_list_of_dicts(dict):
+    list = [{"key": key, "num": value} for key, value in dict.items()]
+
+    list.sort(reverse=True, key=sort_on)
+
+    return list
 
 
 main()
